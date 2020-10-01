a17f90748258d234281ad2506094352c7002d2a5
Subhasis Bhattacharya <subhasis.bhattacharya@gmail.com>
2020-10-01 13:16:57 +0530

    update docker stuff

--- pbs_docker/dock.sh
+++
@@ -1,8 +0,0 @@
-status=`docker inspect -f '{{.State.Status}}' devenv`
-if [ "$status" = "running" ]; then
-	docker-compose exec pbsenv bash
-	exit 0
-elif [ "$status" = "exited" ]; then
-	docker-compose rm -s -f pbsenv
-fi
-docker-compose up -d pbsenv
